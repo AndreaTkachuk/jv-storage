834cc86f36e260c62a1cc12d03ada653db92d3a5
AndreaTkachuk <kovklad@gmail.com>
2025-10-03 22:17:25 +0200

    solution3

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,16 +3,18 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-     private static final int MAX_CAPACITY = 10;
+    private static final int MAX_CAPACITY = 10;
     private final K[] keys;
     private final V[] values;
     private int size;
+
     @SuppressWarnings("unchecked")
     public StorageImpl() {
         keys = (K[]) new Object[MAX_CAPACITY];
         values = (V[]) new Object[MAX_CAPACITY];
         size = 0;
     }
+
     @Override
     public void put(K key, V value) {
         int index = findIndex(key);
@@ -24,11 +26,13 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             size++;
         }
     }
+
     @Override
     public V get(K key) {
         int index = findIndex(key);
         return index != -1 ? values[index] : null;
     }
+    
     @Override
     public int size() {
         return size;
